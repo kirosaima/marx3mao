90d453a88d329181dbe88c7b3e614513f31cec81
Mic Adams <llakema@riseup.net>
2021-03-25 14:43:33 +0000

    fix textlist

--- textlist.php
+++ textlist.php
@@ -10,17 +10,50 @@
 <body>
     <ul>
         <?php
-            $directory = "articles/";
-            if (is_dir($directory)) {
-                if ($opendirectory = opendir($directory)) {
-                    while (($file = readdir($opendirectory)) !== false) {
-                        if (pathinfo($file)['extension'] == "html") {
-                            echo "<li><a href='$directory/$file'>$file</a></li>";
-                        }
-                        closedir($opendirectory);
-                    }
-                }
-            }
+function getFileList($dir)
+{
+  // array to hold return value
+  $retval = [];
+
+  // add trailing slash if missing
+  if(substr($dir, -1) != "/") {
+    $dir .= "/";
+  }
+
+  // open pointer to directory and read list of files
+  $d = @dir($dir) or die("getFileList: Failed opening directory {$dir} for reading");
+  while(FALSE !== ($entry = $d->read())) {
+    // skip hidden files
+    if($entry{0} == ".") continue;
+    if(is_dir("{$dir}{$entry}")) {
+      $retval[] = [
+        'name' => "{$dir}{$entry}/",
+        'type' => filetype("{$dir}{$entry}"),
+        'size' => 0,
+        'lastmod' => filemtime("{$dir}{$entry}")
+      ];
+    } elseif(is_readable("{$dir}{$entry}")) {
+      $retval[] = [
+        'name' => "{$dir}{$entry}",
+        'type' => mime_content_type("{$dir}{$entry}"),
+        'size' => filesize("{$dir}{$entry}"),
+        'lastmod' => filemtime("{$dir}{$entry}")
+      ];
+    }
+  }
+  $d->close();
+
+  return $retval;
+}
+$dirlist = getFileList("articles/");
+foreach ($dirlist as $file) {
+    if($file['type'] != 'text/plain') {
+        continue;
+    }
+    echo "<li><a href=\"articles/",basename($file['name']),"\">",basename($file['name']), "</a></li>\n";
+    //echo $file['name'] . "\n";
+    //echo $file['type'] . "\n";
+}
         ?>
     </ul>
 </body>
